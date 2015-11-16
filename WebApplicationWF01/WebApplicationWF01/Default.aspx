13bab3179544e6bbd25605f588886a53574990f3
jaleskinen <jarmo.leskinen@hepokumpu.com>
2015-11-16 14:00:53 +0200

    16-11-2015 Jarmo Leskinen. Repository created

---
+++ WebApplicationWF01/WebApplicationWF01/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplicationWF01._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>This is my Web Form Page!</p>
+    <p> <% =DateTime.Now %></p>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+    <p>
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </p>
+    
+
+</asp:Content>
---
+++ WebApplicationWF01/WebApplicationWF01/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationWF01
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        /*
+            if (Label1.Text != "Hello World!")
+                Label1.Text = "Hello World!";
+            else
+                Label1.Text = "World Hello";
+        */
+            //Response.Redirect("http://www.opiframe.com");
+
+            Label1.Text = "Server: ";
+            Label1.Text += this.Server.MachineName;
+            Label1.Text += " ScriptTimeout: ";
+            Label1.Text += this.Server.ScriptTimeout;
+            Label1.Text += " LogonUserIdentity: ";
+            Label1.Text += this.Request.LogonUserIdentity;
+            //Label1.Text == " ";
+
+        }
+    }
+}
